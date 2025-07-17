60e83315fdce164fe16fb9d12fabb9d553c0204a
mohammad-salim-23 <mohammadsalim017427@gmail.com>
2025-07-17 17:44:59 +0600

    registration validation add by zod

---
+++ src/components/modles/auth/registerValidation.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const registrationSchema = z.object({
+  name: z.string()
+    .min(1, { message: "Name is required" })
+    .min(2, { message: "Name must be at least 2 characters" }),
+
+  email: z.string()
+    .min(1, { message: "Email is required" })
+    .email({ message: "Invalid email address" }),
+
+  password: z
+    .string()
+    .min(1, { message: "Password is required" })
+    .min(6, { message: "Password must be at least 6 characters" }),
+});
